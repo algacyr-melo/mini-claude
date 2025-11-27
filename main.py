b6abf1ea5131346b0e1eefca22e888387f336bba
Algacyr Melo <alsdmelo@protonmail.com>
2025-11-26 22:32:10 -0300

    feat: implement a feedback loop

--- main.py
+++ main.py
@@ -5,10 +5,8 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
-from functions.get_files_info import schema_get_files_info
-from functions.get_file_content import schema_get_file_content
-from functions.run_python_file import schema_run_python_file
-from functions.write_file import schema_write_file
+from prompts import system_prompt
+from call_function import available_functions, call_function
 
 
 def main():
@@ -35,46 +33,55 @@ def main():
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
 
-    system_prompt = """
-    You are a helpful AI coding agent.
+    iterations = 0
+    while iterations < 20:
+        try:
+            response = client.models.generate_content(
+                model="gemini-2.0-flash-001",
+                contents=messages,
+                config=types.GenerateContentConfig(
+                    tools=[available_functions],
+                    system_instruction=system_prompt,
+                )
+            )
+            for candidate in response.candidates:
+                messages.append(candidate.content)
 
-    When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+            if verbose:
+                print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+                print("Response tokens:", response.usage_metadata.candidates_token_count)
 
-    - List files and directories
-    - Read file contents
-    - Execute Python files with optional arguments
-    - Write or overwrite files
+            if (
+                not response.function_calls
+                and response.text
+            ):
+                print(f"Final response:\n{response.text}")
+                break
 
-    All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
-    """
+            function_responses = []
+            for function_call_part in response.function_calls:
+                function_call_result = call_function(function_call_part, verbose)
+                if(
+                    not function_call_result.parts
+                    or not function_call_result.parts[0].function_response
+                ):
+                    raise Exception("empty function call result")
+                if verbose:
+                    print(f"{function_call_result.parts[0].function_response.response}")
+                function_responses.append(function_call_result.parts[0])
 
-    available_functions = types.Tool(
-        function_declarations=[
-            schema_get_files_info,
-            schema_get_file_content,
-            schema_run_python_file,
-            schema_write_file,
-        ]
-    )
+            for function_response in function_responses:
+                messages.append(
+                    types.Content(
+                        role="user",
+                        parts=[function_response],
+                    )
+                )
 
-    response = client.models.generate_content(
-        model="gemini-2.0-flash-001",
-        contents=messages,
-        config=types.GenerateContentConfig(
-            tools=[available_functions],
-            system_instruction=system_prompt,
-        )
-    )
-    if verbose:
-        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-        print("Response tokens:", response.usage_metadata.candidates_token_count)
+        except Exception as e:
+            return f"Error: {e}"
 
-    if not response.function_calls:
-        return response.text
-
-    for function_call_part in response.function_calls:
-        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
-
+        iterations += 1
 
 if __name__ == "__main__":
     main()
